1468be117396f85aeae78c46cd7bd3c371cbafbd
Ronak Parmar <parmarronak1090@gmail.com>
2025-08-04 01:19:46 +0530

    Feat:changed header color

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
@@ -7,16 +8,29 @@ import { SearchBar } from "./SearchBar";
 import { HeaderActions } from "./HeaderActions";
 import { ReportsDropdown } from "./ReportsDropdown";
 import { Breadcrumbs } from "./breadcrumbs";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
+  const pathname = usePathname();
   return (
-    <div className="h-20  bg-black justify-between flex items-center px-4">
+    <div
+      className={cn(
+        "h-20  bg-black justify-between flex items-center px-4",
+        pathname.includes("client-admin") && "bg-[#235375]"
+      )}
+    >
       <div className="flex gap-4 items-center">
         <Image src="/logo.svg" alt="logo" width={150} height={40} />
 
         <UserProfilePopover />
 
-        <Button className="text-sm font-normal h-9">
+        <Button
+          className={cn(
+            "text-sm font-normal h-9",
+            pathname.includes("client-admin") && "text-white bg-white/10"
+          )}
+        >
           <Image src="/pie.svg" width={20} height={20} alt="pie" />
           <p className="hidden xl:block">Vessels</p>
         </Button>
--- components/common/ReportsDropdown.tsx
+++ components/common/ReportsDropdown.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   DropdownMenu,
@@ -9,8 +10,11 @@ import { ChevronDown, Folder, Heart, Ticket } from "lucide-react";
 import { SearchBar } from "./SearchBar";
 import { HeaderActions } from "./HeaderActions";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export const ReportsDropdown = () => {
+  const pathname = usePathname();
   return (
     <div className="flex gap-3 items-center">
       <SearchBar />
@@ -34,7 +38,12 @@ export const ReportsDropdown = () => {
           <DropdownMenuItem>Custom Report</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-      <Button className="text-sm font-normal h-9">
+      <Button
+        className={cn(
+          "text-sm font-normal h-9",
+          pathname.includes("client-admin") && "text-white bg-white/10"
+        )}
+      >
         <Ticket className="block xl:hidden" />
         <span className="hidden xl:block">Support Ticket </span>
       </Button>
